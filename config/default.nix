d9196e926bc661b75fe9840371a809c4d5d29bdc
tekky02 <tekkykk02@gmail.com>
2025-06-16 00:27:46 +0800

    init

---
+++ config/default.nix
@@ -0,0 +1,4 @@
+{ ... }:
+{
+  imports = [ ./snacks.nix ];
+}
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, nixvim, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      config = import ./config;
+
+      nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+        inherit pkgs;
+        module = config;
+      };
+    in
+    {
+      packages.${system}.default = nvim;
+    };
+}
